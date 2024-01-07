92c27815a45465771d9b73582bf1e8114bb81e15
Edelqueen <78555565+Edelqueen@users.noreply.github.com>
2024-01-07 18:26:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var randomNumber1 = Math.floor((Math.random()*6)+1);
 var randomDiceImg1 = "dice"+randomNumber1+".png";
 var randomNumber2 = Math.floor((Math.random()*6)+1);
 var randomDiceImg2 = "dice"+randomNumber2+".png";
-document.querySelector(".img1").setAttribute("src", ("images/" + randomDiceImg1));
-document.querySelector(".img2").setAttribute("src", ("images/" + randomDiceImg2));
+document.querySelector(".img1").setAttribute("src", (randomDiceImg1));
+document.querySelector(".img2").setAttribute("src", (randomDiceImg2));
 
 if (randomNumber1>randomNumber2){
   document.querySelector("h1").innerHTML = "Player 1 Wins";
